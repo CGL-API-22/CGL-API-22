fd47b1fe02bf726891ec84a2920134174ea68170
maceteli <maceteligolden@gmail.com>
2022-11-13 19:29:34 +0100

    updating package.json

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   apps : [{
-    script: 'src/index.js',
+    script: 'src/index.ts',
     instances: "max",
     env: {
       NODE_ENV: 'development'
--- src/infrastruture/router.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var InvestorRoutes_1 = require("../modules/Auth/InvestorRoutes");
-function setModuleRouters(app) {
-    //Investor
-    app.use('/api/v1/auth', InvestorRoutes_1["default"]);
-}
-exports["default"] = setModuleRouters;
